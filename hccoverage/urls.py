b212eeab0e6f7c86633eddd9a088ebb7c492cbea
Ian Fisher <iafisher@protonmail.com>
2019-03-20 09:16:47 -0400

    Fix admin URL

--- hccoverage/urls.py
+++ hccoverage/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("demos/am-i-covered/admin/", admin.site.urls),
     path("demos/am-i-covered/", include("coverage_app.urls")),
 ]
