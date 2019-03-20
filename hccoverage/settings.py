180fcba68808284d4064533c2131712476d1cf01
Ian Fisher <iafisher@protonmail.com>
2019-03-20 09:11:04 -0400

    Update STATIC_URL setting

--- hccoverage/settings.py
+++ hccoverage/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/demos/am-i-covered/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static_collected")
 
 
