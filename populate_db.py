4f923a56e59209716323b5f32db6cb2fde8444ca
Ian Fisher <iafisher@protonmail.com>
2019-03-27 10:58:34 -0400

    Select a radio button by default and add more appointments

--- populate_db.py
+++ populate_db.py
@@ -22,8 +22,8 @@ kp = Provider.objects.create(name="kp")
 
 aetna.nurses.add(richard, yasmine)
 bcbs.nurses.add(anna, yasmine)
-humana.nurses.add(richard)
-kp.nurses.add(anna, yasmine)
+humana.nurses.add(anna, richard)
+kp.nurses.add(yasmine)
 
 
 Appointment.objects.create(date="2019-03-18", start=600, end=660, nurse=anna)
@@ -36,3 +36,14 @@ Appointment.objects.create(date="2019-03-21", start=900, end=930, nurse=richard)
 Appointment.objects.create(date="2019-03-18", start=720, end=765, nurse=yasmine)
 Appointment.objects.create(date="2019-03-20", start=720, end=765, nurse=yasmine)
 Appointment.objects.create(date="2019-03-22", start=720, end=765, nurse=yasmine)
+
+Appointment.objects.create(date="2019-03-25", start=600, end=660, nurse=anna)
+Appointment.objects.create(date="2019-03-27", start=600, end=660, nurse=anna)
+Appointment.objects.create(date="2019-03-29", start=600, end=660, nurse=anna)
+
+Appointment.objects.create(date="2019-03-26", start=900, end=930, nurse=richard)
+Appointment.objects.create(date="2019-03-28", start=900, end=930, nurse=richard)
+
+Appointment.objects.create(date="2019-03-25", start=720, end=765, nurse=yasmine)
+Appointment.objects.create(date="2019-03-27", start=720, end=765, nurse=yasmine)
+Appointment.objects.create(date="2019-03-29", start=720, end=765, nurse=yasmine)
